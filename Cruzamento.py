fdc4c417c4da5798bb423053238cbcc2bc80d046
Estevão Reis <estevaoreis1587@gmail.com>
2022-07-27 13:41:06 -0300

    Iniciando a lógica do semáforo

---
+++ Cruzamento.py
@@ -0,0 +1,5 @@
+
+class Cruzamento:
+  def __init__(self, smf_principal, smf_auxiliar):
+    self.smf_principal = smf_principal
+    self.smf_auxiliar - smf_auxiliar
---
+++ Semaforo.py
@@ -0,0 +1,39 @@
+from gpiozero import LED
+from time import sleep
+
+
+class Semaforo:
+  def __init__(self, n_semaforo, vermelho, verde, amarelo):
+    self.n_semaforo = n_semaforo
+    self.vermelho = LED(vermelho)
+    self.verde = LED(verde)
+    self.amarelho = LED(amarelo)
+    self.estado = 0
+    self.is_verde = False
+    self.is_vermelho = True
+    self.is_amarelo = False
+
+  def pare(self):
+    self.vermelho.on()
+    self.amarelho.off()
+    self.verde.off()
+    self.is_verde = False
+    self.is_vermelho = True
+    self.is_amarelo = False
+  
+  def atencao(self):
+    self.vermelho.off()
+    self.amarelho.on()
+    self.verde.off()
+    self.is_verde = False
+    self.is_vermelho = False
+    self.is_amarelo = True
+    
+
+  def passe(self):
+    self.vermelho.off()
+    self.amarelho.off()
+    self.verde.on()
+    self.is_verde = True
+    self.is_vermelho = False
+    self.is_amarelo = False
