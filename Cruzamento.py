c5c3bc6802ad0a80cc2fbd137781bad266d3691b
Estevão Reis <estevaoreis1587@gmail.com>
2022-07-28 17:12:12 -0300

    Testando sensor de velocidade

--- Cruzamento.py
+++ Cruzamento.py
@@ -27,7 +27,12 @@ class Cruzamento:
     self.sensor_pass1.when_pressed = self.libera_via_aux
     self.sensor_pass2.when_pressed = self.libera_via_aux
 
-    self.sensor_v1 = SensorVelocidade(sensor_v1_a, sensor_v1_b)
+    self.sensor_v1_a = Button(sensor_v1_a)
+    self.sensor_v1_b = Button(sensor_v1_b)
+    self.sensor_v1_a.when_pressed = self.printaA
+    self.sensor_v1_b.when_pressed = self.printaB
+
+    #self.sensor_v1 = SensorVelocidade(sensor_v1_a, sensor_v1_b)
     self.sensor_v2 = SensorVelocidade(sensor_v2_a, sensor_v2_b)
 
   def controla_semaforos(self):
@@ -103,5 +108,10 @@ class Cruzamento:
       self.is_carro_esperando = True
     else:
       self.is_carro_esperando = False
+
+  def printaA(self):
+    print("CLICOU NO AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
   
+  def printaB(self):
+    print("CLICOU NO BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
   
--- SensorVelocidade.py
+++ SensorVelocidade.py
@@ -1,10 +1,26 @@
 from gpiozero import Button
+import time
 class SensorVelocidade:
   def __init__(self, sensor_v_a, sensor_v_b):
-      self.sensor_v1_a = Button(sensor_v_a)
-      self.sensor_v1_b = Button(sensor_v_b)
+      self.sensor_v_a = Button(sensor_v_a)
+      self.sensor_v_b = Button(sensor_v_b)
+      self.sensor_v_a.when_pressed = self.registra_ti
+      self.sensor_v_b.when_pressed = self.calcula_velocidade
       self.quantidade_carros = 0
       self.velocidade_media = 0
       self.velocidades = []
+      self.ti = 0
+      self.tf = 0  
+      print("Sensor de velocidade criado")   
+
+  def registra_ti(self):
+    self.ti = time.time()
+    print("PREEEEEEEECIONOU o A")
   
 
+  def calcula_velocidade(self):
+    print("PREEEEEEEECIONOU o B")
+    self.tf = time.time()
+    # delta s/ deslta t
+    self.velocidade_media = (1.0/(self.tf - self.ti)) * 3.6
+    print(f'Velocidade media: {self.velocidade_media}km')
